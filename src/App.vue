98ddb8baf26c927eb72c1840fe7c2c48e719c926
okumurakengo <okumurakengo1a@gmail.com>
2019-05-19 19:20:36 +0900

    動作サンプル

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="app" class="container">
+        <h1>My Counter</h1>
+        <hr>
+        <p class="count">{{ count }}</p>
+        <Btn class="blue" @handleCount="doCount('up')">Up</Btn>
+        <Btn class="green" @handleCount="doCount('down')">Down</Btn>
+    </div>
+</template>
+
+<script>
+import Btn from "./components/Btn"
+
+export default {
+    components: {
+        Btn
+    },
+    data () {
+        return {
+            count: 0,
+        }
+    },
+    methods: {
+        doCount(countType) {
+            this.count += (countType === "up" ? 1 : -1)
+        },
+    },
+}
+</script>
+
+<style scope>
+.container {
+    text-align: center;
+    width: 500px;
+}
+
+.count {
+    font-size: 35px;
+    margin: 10px;
+}
+</style>
---
+++ src/components/Btn.vue
@@ -0,0 +1,38 @@
+<template>
+    <button @click="_handleCount">
+        Count <slot></slot>!
+    </button>
+</template>
+
+<script>
+export default {
+    methods: {
+        _handleCount() {
+            this.$emit("handleCount");
+        }
+    }
+}
+</script>
+
+<style scope>
+button {
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 15px;
+    height: 35px;
+    width: 40%;
+    color: #fff;
+    transition: opacity .15s;
+}
+button:hover {
+    opacity: .8;
+}
+button.blue {
+    background-color: #007bff;
+    border-color: #007bff;
+}
+button.green {
+    background-color: #28a745;
+    border-color: #28a745;
+}
+</style>
